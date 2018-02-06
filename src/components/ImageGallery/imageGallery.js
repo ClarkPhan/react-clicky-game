8df18737e43664aa97229fb745728672eb21fd41
Clark <clarkphan@github.com>
2018-02-05 21:37:56 -0800

    made image gallery an extension of component class

--- src/components/ImageGallery/imageGallery.js
+++ src/components/ImageGallery/imageGallery.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const imageGallery = props => (
-  <div className='container'>
-    <div className='row'>
-      <div className='col-md-4'>
-        <div className='thumbnail'>
-          <img src={props.src1 ? props.src1 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+class imageGallery extends Component { 
+  render() {
+    return(
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src1 ? this.props.src1 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
         </div>
-      </div>
-      <div className='col-md-4'>
-        <div className='thumbnail'>
-          <img src={props.src2 ? props.src2 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src2 ? this.props.src2 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
         </div>
-      </div>
-      <div className='col-md-4'>
-        <div className='thumbnail'>
-          <img src={props.src3 ? props.src3 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" class="img-responsive" style={{width: '40%'}}></img>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src3 ? this.props.src3 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)
+    )
+  }
+}
 
 export default imageGallery
