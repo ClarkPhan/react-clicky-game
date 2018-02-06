ae53b14f32e653f8aa88801158b1a7081ef2e892
Clark <clarkphan@github.com>
2018-02-05 23:23:19 -0800

    nav extends component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Nav.css";
 
-const Nav = () => (
-  <nav className="navbar navbar-inverse navbar-fixed-top">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <a href="/" className="navbar-brand" id="brand">
-          Clicky Game!
-        </a>
-        <h4 className="nav navbar-text navbar-center">Click an image to begin!</h4> 
-        <h4 className="nav navbar-text navbar-right">Score: 0 | Top Score: 0</h4> 
+class Nav extends Component {
+  state = {
+    hiScore : 0
+  } 
+  render () {
+    return  (
+      <div>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a href="/" className="navbar-brand" id="brand">
+                Clicky Game!
+              </a>
+              <h4 className="nav navbar-text navbar-center">Click an image to begin!</h4> 
+              <h4 className="nav navbar-text navbar-right">Score: {this.props.score} | Top Score: 0</h4> 
+            </div>
+          </div>
+        </nav>
       </div>
-    </div>
-  </nav>
-);
+    );
+  };
+}
 
 export default Nav;
