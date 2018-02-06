9b9161b0d0a7732e153b30b9f2688a6921331ced
Clark <clarkphan@github.com>
2018-02-05 21:41:29 -0800

    refactored image gallery component

--- src/App.js
+++ src/App.js
@@ -13,17 +13,12 @@ class App extends Component {
           src1="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_13-256.png"
           src2="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_14-256.png"
           src3="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_4-256.png"
-        />
-
-        <ImageGallery
-          src1="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_5-256.png"
-          src2="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_2-256.png"
-          src3="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_11-256.png"
-        />
-        <ImageGallery
-          src1="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_9-256.png"
-          src2="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_10-256.png"
-          src3="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_17-256.png"
+          src4="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_5-256.png"
+          src5="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_2-256.png"
+          src6="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_11-256.png"
+          src7="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_9-256.png"
+          src8="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_10-256.png"
+          src9="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_17-256.png"
         />
       </div>
     )
--- src/components/ImageGallery/imageGallery.js
+++ src/components/ImageGallery/imageGallery.js
@@ -21,6 +21,40 @@ class imageGallery extends Component {
           </div>
         </div>
       </div>
+      <div className='row'>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src4 ? this.props.src4 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src5 ? this.props.src5 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src6 ? this.props.src6 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.sr7 ? this.props.src7 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src8 ? this.props.src8 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.props.src9 ? this.props.src9 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" class="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+      </div>
     </div>
     )
   }
