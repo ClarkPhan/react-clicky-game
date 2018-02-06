62e432f4146e1125f3c7c5c33c4e0a8b13e9b610
Clark <clarkphan@github.com>
2018-02-05 17:44:17 -0800

    basic nav bar rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Bootstrap from './components/bootstrap';
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
     return (
-      <Bootstrap></Bootstrap>
+      <Nav></Nav>
     );
   }
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => (
+  <nav className="navbar navbar-inverse navbar-top">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a href="/" className="navbar-brand" id="brand">
+          Clicky Game!
+        </a>
+        <h4 class="nav navbar-text navbar-center">Click an image to begin!</h4> 
+        <h4 class="nav navbar-text navbar-right">Score: 0 | Top Score: 0</h4> 
+      </div>
+    </div>
+  </nav>
+);
+
+export default Nav;
