c6c090dd35f2882d1d2852518abcf2adce9b9005
Clark <clarkphan@github.com>
2018-02-05 22:03:42 -0800

    image gallery component rendering is now dependent on this.state.images in imageGallery class

--- src/components/ImageGallery/imageGallery.js
+++ src/components/ImageGallery/imageGallery.js
@@ -39,57 +39,58 @@ class imageGallery extends Component {
     console.log(this.state.images);
     console.log(this.shuffle(this.state.images));
   }
+
   render() {
     return(
     <div className='container'>
       <div className='row'>
         <div className='col-md-4'>
           <div className='thumbnail'>
-            <img src={this.props.src1 ? this.props.src1 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
+            <img src={this.state.images[0] ? this.state.images[0] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
           </div>
         </div>
         <div className='col-md-4'>
           <div className='thumbnail'>
-            <img src={this.props.src2 ? this.props.src2 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
+            <img src={this.state.images[1] ? this.state.images[1] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
           </div>
         </div>
         <div className='col-md-4'>
           <div className='thumbnail'>
-            <img src={this.props.src3 ? this.props.src3 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" className="img-responsive" style={{width: '40%'}}></img>
-          </div>
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col-md-4'>
-          <div className='thumbnail'>
-            <img src={this.props.src4 ? this.props.src4 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
-          </div>
-        </div>
-        <div className='col-md-4'>
-          <div className='thumbnail'>
-            <img src={this.props.src5 ? this.props.src5 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
-          </div>
-        </div>
-        <div className='col-md-4'>
-          <div className='thumbnail'>
-            <img src={this.props.src6 ? this.props.src6 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" className="img-responsive" style={{width: '40%'}}></img>
+            <img src={this.state.images[2] ? this.state.images[2] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" className="img-responsive" style={{width: '40%'}}></img>
           </div>
         </div>
       </div>
       <div className='row'>
         <div className='col-md-4'>
           <div className='thumbnail'>
-            <img src={this.props.sr7 ? this.props.src7 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
+            <img src={this.state.images[3] ? this.state.images[3]: 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
           </div>
         </div>
         <div className='col-md-4'>
           <div className='thumbnail'>
-            <img src={this.props.src8 ? this.props.src8 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
+            <img src={this.state.images[4] ? this.state.images[4] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
           </div>
         </div>
         <div className='col-md-4'>
           <div className='thumbnail'>
-            <img src={this.props.src9 ? this.props.src9 : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" className="img-responsive" style={{width: '40%'}}></img>
+            <img src={this.state.images[5] ? this.state.images[5] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" className="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.state.images[6] ? this.state.images[6] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.state.images[7] ? this.state.images[7] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'} alt="" className="img-responsive" style={{width: '40%'}}></img>
+          </div>
+        </div>
+        <div className='col-md-4'>
+          <div className='thumbnail'>
+            <img src={this.state.images[8] ? this.state.images[8] : 'https://cdn4.iconfinder.com/data/icons/longico/224/longico-23-512.png'}  alt="" className="img-responsive" style={{width: '40%'}}></img>
           </div>
         </div>
       </div>
